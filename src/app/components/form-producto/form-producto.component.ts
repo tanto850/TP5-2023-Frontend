d35b59f266d06363a098787f13f06b2bc8a6132f
Antonela Flores <totte_30_98@hotmail.com>
2023-06-13 19:47:05 -0300

    cuarto commit

--- src/app/components/form-producto/form-producto.component.ts
+++ src/app/components/form-producto/form-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Producto } from 'src/app/models/producto';
 import { ProductoService } from 'src/app/services/producto.service';
 
@@ -10,13 +11,35 @@ import { ProductoService } from 'src/app/services/producto.service';
 export class FormProductoComponent implements OnInit {
 
   producto!:Producto;
+  accion:string="";
 
-  constructor(private productoService:ProductoService) {
+  constructor(private productoService:ProductoService,
+              private activatedRoute : ActivatedRoute) {
     this.producto = new Producto();
    }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe(params =>{
+      if (params['id'] == "0"){
+        this.accion = "new";
+      }else {
+        this.accion = "update";
+        this.cargarProducto(params['id']);
+      }
+    });
   }
   
+  cargarProducto(id:String){
+    this.productoService.getProducto(id).subscribe(
+      result =>{
+        console.log(result);
+        Object.assign(this.producto, result);
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+  }
 
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -14,22 +14,15 @@ export class ProductoService {
 
   }
 
-  getProducto(param : Producto):Observable<any>{
+  getProducto(id:String):Observable<any>{
 
     const httpOption = {
       headers: new HttpHeaders({
-        'content-type' : 'application/x-www-form-urlencoded',
-        'X-RapidAPI-Key': '8034f52893msha40e8a7aaa57215p155a6fjsn2294956a4b4a',
-        'X-RapidAPI-Host': 'community-neutrino-currency-conversion.p.rapidapi.com'
       })
     }
 
     const body = new HttpParams()
-//      .set('from-value', param.valor)
-//        .set('from-type', param.deTipo)
-//          .set('to_type', param.aTipo);
-
-    return this.http.post(this.direccion ,body ,httpOption);
+    return this.http.get(this.direccion + id ,httpOption);
   }
 
   getProductos():Observable<any>{
